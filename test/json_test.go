0d732465e62ab84e7af8e72305112b6e17aa9c10
frankykoalo <rui.liu@viewsources.com>
2021-09-24 13:35:02 +0800

    update http to https

--- test/json_test.go
+++ test/json_test.go
@@ -2,8 +2,10 @@ package test
 
 import (
 	"bytes"
+	"crypto/tls"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"testing"
 	"time"
@@ -11,15 +13,15 @@ import (
 
 func TestJson(t *testing.T) {
 	fmt.Println(time.Now())
-	heartbeatInterval := time.NewTicker(5 * time.Second)
+	heartbeatInterval := time.NewTicker(3 * time.Second)
 
 	defer heartbeatInterval.Stop()
-	tps := 2000
 	for {
-		tps++
+		rand.Seed(time.Now().Unix())
+		tps := rand.Intn(500)
 		select {
 		case <-heartbeatInterval.C:
-			url := fmt.Sprintf("http://%s/api/1/explorer/updateTps", "127.0.0.1:10232")
+			url := fmt.Sprintf("https://%s/api/1/explorer/updateTps", "127.0.0.1:16666")
 			body := fmt.Sprintf("{\"tps\":\"%d\"}", tps)
 
 			fmt.Printf(" will send data to url : %s , tps : %d", url, tps)
@@ -29,7 +31,13 @@ func TestJson(t *testing.T) {
 				continue
 			}
 			req.Header.Set("Content-Type", "application/json")
-			client := &http.Client{}
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			}
+			client := &http.Client{Transport: tr}
+			//client := &http.Client{}
 			res, err := client.Do(req)
 			if err != nil {
 				log.Fatalf(" can't notification data to DAG-Explorer, err: %s", err.Error())
